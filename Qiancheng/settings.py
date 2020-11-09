b5b590990d41c007620d33d8f5fff35b09388c26
ggq18663278150@sina.com <5515225gg5>
2020-11-09 12:25:48 +0800

    修改日志的名字

--- Qiancheng/settings.py
+++ Qiancheng/settings.py
@@ -146,7 +146,7 @@ configure_logging(install_root_handler=False)
 logging.basicConfig(
     level=logging.DEBUG,
     handlers=[
-        TimedRotatingFileHandler(filename='logs/LiePin.log', encoding='utf-8', when="D", interval=1)],
+        TimedRotatingFileHandler(filename='logs/Qiancheng.log', encoding='utf-8', when="D", interval=1)],
     format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s',
     datefmt='%a, %d %b %Y %H:%M:%S',
 )
